8a8e3bd0f7fc36d694fcb8c2be2592e13c59eb5a
enikebraimoh <richardbraimoh@gmail.com>
2023-02-16 03:08:03 +0100

    final touches

--- namer/lib/main.dart
+++ namer/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'My Namer App',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
